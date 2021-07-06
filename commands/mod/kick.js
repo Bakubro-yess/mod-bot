150f70f107be8d82964f340d6746a7637ff84de8
Braxy <81979291+Braxy-1@users.noreply.github.com>
2021-07-06 19:50:48 +0530

    NOT DONE YET BRUH

--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -14,3 +14,17 @@ module.exports = {
         if (member.kick()) return message.reply(`I have kicked <@${member.user.id}>!`)
     }
 }
+module.exports = {
+	name: 'kick',
+	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
+	execute(message) {
+        const member = message.mentions.members.first()
+        if(!member) return message.reply('Mention a member')
+        if (member === message.member) return message.reply('You cannot kick yourself bruh'); 
+        if (member.roles.highest.position >= message.member.roles.highest.position) 
+        return message.reply('Not allowed to kick someone with a role higher or equal to ya');
+  
+  member.kick()
+
+	},
+};
